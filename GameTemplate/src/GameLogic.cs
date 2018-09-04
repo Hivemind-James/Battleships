dc82d84b0a75c093e91c1fc03b6066ee341525db
jlloyddominik <101663792@student.swin.edu.au>
2018-09-04 15:12:24 +1000

    add validated code from instructor

--- GameTemplate/src/GameLogic.cs
+++ GameTemplate/src/GameLogic.cs
@@ -1,34 +1,46 @@
-﻿using System;
+
+using Microsoft.VisualBasic;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+//using System.Data;
+using System.Diagnostics;
 using SwinGameSDK;
-using static SwinGameSDK.SwinGame; // requires mcs version 4+, 
-// using SwinGameSDK.SwinGame; // requires mcs version 4+, 
+using System.Runtime.ExceptionServices;
+using System.Security;
 
-namespace Battleships
+static class GameLogic
 {
-    static class GameLogic
-    {
-        public static void Main()
-        {
-            // Opens a new Graphics Window
-            SwinGame.OpenGraphicsWindow("Battle Ships", 800, 600);
+    [HandleProcessCorruptedStateExceptions]
+    [SecurityCritical]
+    public static int Main()
+	{
+		//Opens a new Graphics Window
+		SwinGame.OpenGraphicsWindow("Battle Ships", 800, 600);
 
-            // Load Resources
-            GameResources.LoadResources();
+		//Load Resources
+		GameResources.LoadResources();
 
-            SwinGame.PlayMusic(GameResources.GameMusic("Background"));
+		SwinGame.PlayMusic(GameResources.GameMusic("Background"));
 
-            // Game Loop
-            do
-            {
-                GameController.HandleUserInput();
-                GameController.DrawScreen();
-            }
-            while (!SwinGame.WindowCloseRequested() == true | GameController.CurrentState == GameState.Quitting);
+		//Game Loop
+		do {
+			GameController.HandleUserInput();
+			GameController.DrawScreen();
+		} while (!(SwinGame.WindowCloseRequested() == true | GameController.CurrentState == GameState.Quitting));
 
-            SwinGame.StopMusic();
+		SwinGame.StopMusic();
 
-            // Free Resources and Close Audio, to end the program.
+        //Free Resources and Close Audio, to end the program.
+        try {
             GameResources.FreeResources();
+        } catch (Exception e)
+        {
+            System.Console.WriteLine("The following exception is due to .NET 4+");
+            System.Console.WriteLine(e.Message);
+            return 1;
         }
-    }
+
+        return 0;
+	}
 }
